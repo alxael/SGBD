25b49b00ddcdda83291c60d86821898cfaa6f8e5
alex-aelenei <alex.aelenei@aelsoftware.com>
2024-12-15 22:05:06 +0200

    feat: add lab 7 homework

---
+++ teme/7/query_2.sql
@@ -0,0 +1,28 @@
+INSERT INTO CINEMA_REVENUE (
+    SELECT
+        C.CINEMA_ID,
+        C.NAME,
+        SUM(SP.REVENUE) AS REVENUE
+    FROM
+        (
+            SELECT
+                S.SCREENING_ID,
+                S.AUDITORIUM_ID,
+                COUNT(R.SEAT_ID) * S.PRICE AS REVENUE
+            FROM
+                SCREENING   S
+                INNER JOIN RESERVATION R
+                ON R.SCREENING_ID = S.SCREENING_ID
+            GROUP BY
+                S.SCREENING_ID,
+                S.PRICE,
+                S.AUDITORIUM_ID
+        )          SP
+        INNER JOIN AUDITORIUM A
+        ON A.AUDITORIUM_ID = SP.AUDITORIUM_ID
+        INNER JOIN CINEMA C
+        ON C.CINEMA_ID = A.CINEMA_ID
+    GROUP BY
+        C.CINEMA_ID,
+        C.NAME
+);
