1c76030603a6e67e017b1bddba193638605c867e
alex-aelenei <alex.aelenei@aelsoftware.com>
2025-01-10 15:53:13 +0200

    feat: add task 6

--- proiect/src/test-data-auto.sql
+++ proiect/src/test-data-auto.sql
@@ -1,26 +1,57 @@
+CREATE OR REPLACE PROCEDURE GENERARE_REZERVARE_PENTRU_CLIENT (
+    ID_CLIENT_PARAM IN CLIENTI.ID_CLIENT%TYPE
+) AS
+    ID_DIFUZARE_GENERAT DIFUZARI.ID_DIFUZARE%TYPE;
+    DIFUZARE            DIFUZARI%ROWTYPE;
+    ID_LOC              LOCURI.ID_LOC%TYPE;
+    ZI_REZERVARE        NUMBER(10);
+BEGIN
+    ID_DIFUZARE_GENERAT := ROUND(DBMS_RANDOM.VALUE(1, 50));
+    SELECT
+        * INTO DIFUZARE
+    FROM
+        DIFUZARI D
+    WHERE
+        D.ID_DIFUZARE = ID_DIFUZARE_GENERAT;
+    ID_LOC := ROUND(DBMS_RANDOM.VALUE(1, 30));
+    ID_LOC := ID_LOC + (DIFUZARE.ID_SALA - 1) * 30;
+    ZI_REZERVARE := ROUND(DBMS_RANDOM.VALUE(1, TO_NUMBER(TO_CHAR(DIFUZARE.DATA, 'dd'))));
+    INSERT INTO REZERVARI VALUES (
+        ID_LOC,
+        DIFUZARE.ID_DIFUZARE,
+        ID_CLIENT_PARAM,
+        TO_DATE('2025/09/'
+                || ZI_REZERVARE, 'yyyy/mm/dd')
+    );
+END GENERARE_REZERVARE_PENTRU_CLIENT;
+/
+
 DECLARE
-    ID_LOC                 NUMBER;
-    COD_LOC                VARCHAR2(255);
-    QUERY_INSERT_LOC       VARCHAR2(255);
-    REZERVARI_PER_CLIENT   NUMBER;
-    ID_DIFUZARE_REZERVARE  NUMBER;
-    ID_SALA_REZERVARE      NUMBER;
-    ID_LOC_REZERVARE       NUMBER;
-    QUERY_INSERT_REZERVARE VARCHAR2(255);
+    LOCURI_PER_SALA          NUMBER(10) := 30;
+    REZERVARI_PER_CLIENT_MAX NUMBER(10) := 5;
+    REZERVARI_PER_CLIENT     NUMBER(10);
+    ZI_REZERVARE             NUMBER(10);
+    ID_LOC                   LOCURI.ID_LOC%TYPE;
+    COD_LOC                  LOCURI.COD%TYPE;
+    ID_DIFUZARE_REZERVARE    REZERVARI.ID_DIFUZARE%TYPE;
+    ID_SALA_REZERVARE        DIFUZARI.ID_SALA%TYPE;
+    ID_LOC_REZERVARE         REZERVARI.ID_LOC%TYPE;
+    QUERY_INSERT_REZERVARE   VARCHAR2(255);
+    QUERY_INSERT_LOC         VARCHAR2(255);
 BEGIN
     FOR DATE_SALA IN (
         SELECT
             *
         FROM
-            SALA
+            SALI
     ) LOOP
-        FOR INDEX_LOC IN 1..CONSTANTE_GLOBALE.LOCURI_PER_SALA LOOP
+        FOR INDEX_LOC IN 1..LOCURI_PER_SALA LOOP
             COD_LOC := 'LOC-'
                        || DATE_SALA.ID_SALA
                        || '-'
                        || INDEX_LOC;
-            ID_LOC := (DATE_SALA.ID_SALA - 1) * CONSTANTE_GLOBALE.LOCURI_PER_SALA + INDEX_LOC;
-            QUERY_INSERT_LOC := 'INSERT INTO LOC VALUES('
+            ID_LOC := (DATE_SALA.ID_SALA - 1) * LOCURI_PER_SALA + INDEX_LOC;
+            QUERY_INSERT_LOC := 'INSERT INTO LOCURI VALUES('
                                 || ID_LOC
                                 || ','
                                 || DATE_SALA.ID_SALA
@@ -31,7 +62,7 @@ BEGIN
                 EXECUTE IMMEDIATE QUERY_INSERT_LOC;
             EXCEPTION
                 WHEN OTHERS THEN
-                    DBMS_OUTPUT.PUT_LINE('Se incearca inserarea unui loc duplicat. Sarim peste.');
+                    DBMS_OUTPUT.PUT_LINE('Se incearca inserarea unui loc invalid. Sarim peste.');
             END;
         END LOOP;
     END LOOP;
@@ -40,30 +71,15 @@ BEGIN
         SELECT
             *
         FROM
-            CLIENT
+            CLIENTI
     ) LOOP
-        REZERVARI_PER_CLIENT := ROUND(DBMS_RANDOM.VALUE(1, CONSTANTE_GLOBALE.REZERVARI_PER_CLIENT_MAX));
+        REZERVARI_PER_CLIENT := ROUND(DBMS_RANDOM.VALUE(1, REZERVARI_PER_CLIENT_MAX));
         FOR INDEX_REZERVARE IN 1..REZERVARI_PER_CLIENT LOOP
-            ID_DIFUZARE_REZERVARE := ROUND(DBMS_RANDOM.VALUE(1, CONSTANTE_GLOBALE.NUMAR_DIFUZARI));
-            SELECT
-                D.ID_SALA INTO ID_SALA_REZERVARE
-            FROM
-                DIFUZARE D
-            WHERE
-                D.ID_DIFUZARE = ID_DIFUZARE_REZERVARE;
-            ID_LOC_REZERVARE := (ID_SALA_REZERVARE - 1) * CONSTANTE_GLOBALE.LOCURI_PER_SALA + ROUND(DBMS_RANDOM.VALUE(1, CONSTANTE_GLOBALE.LOCURI_PER_SALA));
-            QUERY_INSERT_REZERVARE := 'INSERT INTO REZERVARE VALUES ('
-                                      || ID_LOC_REZERVARE
-                                      || ','
-                                      || ID_DIFUZARE_REZERVARE
-                                      || ','
-                                      || DATE_CLIENT.ID_CLIENT
-                                      || ')';
             BEGIN
-                EXECUTE IMMEDIATE QUERY_INSERT_REZERVARE;
+                GENERARE_REZERVARE_PENTRU_CLIENT(DATE_CLIENT.ID_CLIENT);
             EXCEPTION
                 WHEN OTHERS THEN
-                    DBMS_OUTPUT.PUT_LINE('Se incearca inserarea unei rezervari duplicat. Sarim peste.');
+                    DBMS_OUTPUT.PUT_LINE('Se incearca inserarea unei rezervari invalide. Sarim peste.');
             END;
         END LOOP;
     END LOOP;
